458f99de8bea6a900bdeee70fed9d5bf981bfcf3
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2023-04-04 11:48:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocktail",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App/>);
